b53e1fbdc2e862e8fe36c8e6ba0cba30ebcb83e4
unknown <alexaraedickenson@gmail.com>
2023-05-09 11:35:47 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+class Casting {
+    public static void main(String[] args) {
+        double myDouble = 112.35;
+        int myInt = (int) myDouble;
+        System.out.println("myDouble: " + myDouble);
+        System.out.println("myInt: " + myInt);
+        String myString = "49";
+        int myInt2 = Integer.parseInt(myString);
+        System.out.println("myString: " + myString);
+        System.out.println("myInt: " + myInt2);
+    }
+}
